d053872e13370fcaef647286fd359482d6140f5e
Ranapop <roxy_rox1995@yahoo.com>
2019-11-03 13:56:52 +0200

    Create buyer.c

---
+++ CP/lab5/car-shop/buyer.c
@@ -0,0 +1,25 @@
+//
+// Created by roxanappop on 11/1/2019.
+//
+#include <stdio.h>
+
+void inputPersonalData(char firstName[], char lastName[], char phoneNumber[], char address[]) {
+    // Input personal data
+    printf("Please input your data\n");
+    printf("---First name:\n");
+    gets(firstName);
+    printf("---Last name:\n");
+    gets(lastName);
+    printf("---Phone number\n");
+    gets(phoneNumber);
+    printf("---Address\n");
+    gets(address);
+}
+
+
+void displayPersonalData(char firstName[], char lastName[], char phoneNumber[], char address[]) {
+    printf("Customer data:\n");
+    printf("-name: %s %s\n", firstName, lastName);
+    printf("-phone number: %s\n", phoneNumber);
+    printf("-address: %s\n", address);
+}
