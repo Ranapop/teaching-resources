40f5e8d3929d1cd1c1879ed7901c42934d144b16
Ranapop <roxy_rox1995@yahoo.com>
2019-11-03 13:57:13 +0200

    Create buyer.h

---
+++ CP/lab5/car-shop/buyer.h
@@ -0,0 +1,11 @@
+//
+// Created by roxanappop on 11/1/2019.
+//
+
+#ifndef DUMMY_REPO_BUYER_H
+#define DUMMY_REPO_BUYER_H
+
+void inputPersonalData(char firstName[], char lastName[], char phoneNumber[], char address[]);
+void displayPersonalData(char firstName[], char lastName[], char phoneNumber[], char address[]);
+
+#endif //DUMMY_REPO_BUYER_H
