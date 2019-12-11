ce6591bb28bccd3d2ec8ad11b365cbaa6d317e93
Ranapop <roxy_rox1995@yahoo.com>
2019-12-11 22:18:05 +0200

    Add files via upload

---
+++ CP/lab8/car-shop/buyer.h
@@ -0,0 +1,13 @@
+//
+// Created by roxanappop on 10/31/2019.
+//
+
+#ifndef CAR_SHOP_BUYER_H
+#define CAR_SHOP_BUYER_H
+
+//function prototypes
+
+void inputPersonalData(char firstName[], char lastName[], char phoneNumber[], char address[]);
+void displayPersonalData(char firstName[], char lastName[], char phoneNumber[], char address[]);
+
+#endif //CAR_SHOP_BUYER_H
